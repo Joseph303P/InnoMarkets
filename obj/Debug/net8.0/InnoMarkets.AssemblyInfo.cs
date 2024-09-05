9a437ea20ed1b95237c1b654c6570845f7ee570c
Joseph <155230237+Joseph303P@users.noreply.github.com>
2024-09-05 17:49:42 -0600

    Eliminacion de carpeta servicio

--- obj/Debug/net8.0/InnoMarkets.AssemblyInfo.cs
+++ obj/Debug/net8.0/InnoMarkets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InnoMarkets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d66001245b0c8ccec613ae3a442c83e7ed7efb3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("InnoMarkets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InnoMarkets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
