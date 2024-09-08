b0e1d834a19adc4e478f897a3ce3132e76cd6c54
Joseph <155230237+Joseph303P@users.noreply.github.com>
2024-09-07 18:13:52 -0600

    .

--- obj/Debug/net8.0/InnoMarkets.AssemblyInfo.cs
+++ obj/Debug/net8.0/InnoMarkets.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InnoMarkets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66018845ee8ab8a5d4f449a0bf19cc39c9fe6854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc942ac2becca69397e9978ce97a57bc1ed6b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("InnoMarkets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InnoMarkets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
